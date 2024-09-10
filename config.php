ca24eac23ce779064ebf496a0174a4153bfd4615
Łukasz J vel Espectro <72676549+espectroPW@users.noreply.github.com>
2024-09-10 12:29:02 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 //serpapi key
-define("API_KEY1","1df5a129f4f9e77b2d8d8615516da7dc6d4e45b6e8175bc39b7313a7a0165e6b");
-define("API_KEY2","4e32d51482295f943f60ce4dc339bc1cbac17a8535afe82f017248f6309f1585");
+define("API_KEY1","");
+define("API_KEY2","");
 
 
